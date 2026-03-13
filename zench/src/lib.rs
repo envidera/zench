ab4994ee143393ea56467add20b8b79866448def
anstk <andrio@envidera.com>
2026-03-12 23:19:22 -0300

    fix: include README

--- zench/src/lib.rs
+++ zench/src/lib.rs
@@ -1,5 +1,5 @@
 #![allow(clippy::let_and_return)]
-#![doc = include_str!("../../README.md")]
+#![doc = include_str!(concat!("../", std::env!("CARGO_PKG_README")))]
 //!
 //! # Item Details
 //! ## Ready-to-use
